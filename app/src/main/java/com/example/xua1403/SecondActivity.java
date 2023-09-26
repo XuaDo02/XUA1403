f4133c03766ab0a42b91a3dbcf06c4dbcc420b85
AdminXua <dothixua20020312@gmail.com>
2023-09-26 10:17:45 +0700

    cardView and Button

---
+++ app/src/main/java/com/example/xua1403/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.xua1403;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
